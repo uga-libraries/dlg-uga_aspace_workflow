bb6e55471a0d69c4261ce44ad9dd1d89bc88bedc
cs49623 <cschmidtuga@gmail.com>
2023-01-04 15:23:10 -0500

    Getting top containers, struggling to connect to archival objects

--- as_export.py
+++ as_export.py
@@ -1,24 +1,49 @@
+import json
+import asnake.logging as logging
+
 from secrets import *
 from asnake.client import ASnakeClient
 from asnake.client.web_client import ASnakeAuthError
 
-# aspace = ASpace(baseurl=as_api, username=as_un, password=as_pw)
-client = ASnakeClient(baseurl=as_api, username=as_un, password=as_pw)
-client.authorize()
-
 # 1st step - user login to ASpace with creds
 
+client = ASnakeClient(baseurl=as_api, username=as_un, password=as_pw)
+# logging.setup_logging(level='DEBUG')
+try:
+    client.authorize()
+except ASnakeAuthError as e:
+    error_message = ""
+    if ":" in str(e):
+        error_divided = str(e).split(":")
+        for line in error_divided:
+            error_message += line + "\n"
+    print(error_message)
+
 # 2nd step - user inputs a spreadsheet of top containers with barcodes or top_container ASpace URI
-# search_tcs = client.get("repositories/4/top_containers/search", params={"q": f"barcode_u_sstr:{32108050893687}"})
-# print(search_tcs.text)
+
 
 
 # 3rd step - code takes spreadsheet, searches for top containers on ASpace
 
+barcode = 32108050893687
+search_tcs = json.loads(client.get("repositories/4/top_containers/search",
+                                   params={"q": f"barcode_u_sstr:{barcode}",
+                                           "type[]": "['top_container']"}).text)
+tc_json = json.loads(search_tcs['response']['docs'][0]['json'])
+print(tc_json['collection'])
+results = [result['uri'] for result in search_tcs['response']['docs']]
+if len(results) > 1:
+    print('Multiple results found with same barcode')
+    print(results)
+elif len(results) == 0:
+    print(f'No results found for {barcode}')
+else:
+    print(results)
+
 # 4th step - get archival objects associated with top containers - use collection ref (URI) to narrow searching for archival objects within collection
 # use https://archivesspace.github.io/archivesspace/api/?shell#fetch-tree-information-for-the-top-level-resource-record - go through each archival object?
-
-
-barcode = "32108050893687"
-get_tc = client.get("/repositories/4/top_containers/45245")
-print(get_tc.text)
+for tc_uri in results:
+    print(tc_uri)
+    search_aos = client.get_paged(f'repositories/4/search', params={"filter_term[]": f'top_container_uri_u_sstr: {tc_uri}', 'type': ['archival_object']})
+    ao_results = [result for result in search_aos]
+    print(len(ao_results))  # 322873 - it's grabbing all archival objects

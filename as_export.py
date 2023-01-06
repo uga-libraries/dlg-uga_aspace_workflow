b3adbdcc19dd11355743d6d5c4c65b8e0a276551
crugas <cschmidtuga@gmail.com>
2023-01-06 14:26:54 -0500

    Starting to build unittest

--- as_export.py
+++ as_export.py
@@ -5,46 +5,66 @@ from secrets import *
 from asnake.client import ASnakeClient
 from asnake.client.web_client import ASnakeAuthError
 
-# 1st step - user login to ASpace with creds
 
-client = ASnakeClient(baseurl=as_api, username=as_un, password=as_pw)
-# logging.setup_logging(level='DEBUG')
-try:
-    client.authorize()
-except ASnakeAuthError as e:
-    error_message = ''
-    if ':' in str(e):
-        error_divided = str(e).split(":")
-        for line in error_divided:
-            error_message += line + "\n"
-    print(error_message)
+class ASpace:
 
-# 2nd step - user inputs a spreadsheet of top containers with barcodes or top_container ASpace URI
+    def __init__(self, username, password, api):
+        self.username = username
+        """str: ArchivesSpace username provided by user"""
+        self.password = password
+        """str: ArchivesSpace password provided by user"""
+        self.api = api
+        """str: ArchivesSpace API URL provided by user"""
+        self.client = None
+        """str: ArchivesSnake client as created through ASnakeClient"""
 
+    # 1st step - user login to ASpace with creds
 
+    def aspace_login(self):
 
-# 3rd step - code takes spreadsheet, searches for top containers on ASpace
+        # logging.setup_logging(level='DEBUG')
+        try:
+            self.client = ASnakeClient(baseurl=as_api, username=as_un, password=as_pw)
+            self.client.authorize()
+            status = 200
+        except ASnakeAuthError as e:
+            print(e)
+            error_message = ''
+            if ':' in str(e):
+                error_divided = str(e).split(":")
+                status = int(error_divided[1])
+                for line in error_divided:
+                    error_message += line + "\n"
+            print(error_message)
+        return status
 
-barcode = 32108050893687
-repository = 4
-search_tcs = json.loads(client.get(f'repositories/{repository}/top_containers/search',
-                                   params={'q': f'barcode_u_sstr:{barcode}',
-                                           'type[]': '["top_container"]'}).text)
-tc_json = json.loads(search_tcs['response']['docs'][0]['json'])
-print(tc_json['collection'])
-results = [result['uri'] for result in search_tcs['response']['docs']]
-if len(results) > 1:
-    print('Multiple results found with same barcode')
-    print(results)
-elif len(results) == 0:
-    print(f'No results found for {barcode}')
-else:
-    print(results)
+    # 2nd step - user inputs a spreadsheet of top containers with barcodes or top_container ASpace URI
 
-# 4th step - get archival objects associated with top containers - use collection ref (URI) to narrow searching for archival objects within collection
-# use https://archivesspace.github.io/archivesspace/api/?shell#fetch-tree-information-for-the-top-level-resource-record - go through each archival object?
-for tc_uri in results:
-    print(tc_uri)
-    search_aos = client.get_paged(f'repositories/4/search', params={'q': f'{barcode}', 'type': ['archival_object']})
-    ao_results = [result for result in search_aos]
-    print(len(ao_results))  # This seems to grab all 27 associated archival objects
+    # 3rd step - code takes spreadsheet, searches for top containers on ASpace
+
+    def get_tcuri(self):
+        barcode = 32108050893687
+        repository = 4
+        search_tcs = json.loads(self.client.get(f'repositories/{repository}/top_containers/search',
+                                                params={'q': f'barcode_u_sstr:{barcode}',
+                                                        'type[]': '["top_container"]'}).text)
+        tc_json = json.loads(search_tcs['response']['docs'][0]['json'])
+        print(tc_json['collection'])
+        results = [result['uri'] for result in search_tcs['response']['docs']]
+        if len(results) > 1:
+            print('Multiple results found with same barcode')
+            print(results)
+        elif len(results) == 0:
+            print(f'No results found for {barcode}')
+        else:
+            print(results)
+
+    # 4th step - get archival objects associated with top containers - use collection ref (URI) to narrow searching for archival objects within collection
+    # use https://archivesspace.github.io/archivesspace/api/?shell#fetch-tree-information-for-the-top-level-resource-record - go through each archival object?
+
+    def get_archobjs(self, barcode, repository):
+        barcode = 32108050893687  # use these for unittests
+        repository = 4  # use these for unittests
+        search_aos = self.client.get_paged(f'repositories/{repository}/search', params={'q': f'{barcode}', 'type': ['archival_object']})
+        ao_results = [result for result in search_aos]
+        print(len(ao_results), ao_results)  # This seems to grab all 27 associated archival objects
--- export_tests.py
+++ export_tests.py
@@ -1,9 +1,17 @@
 import unittest
+import as_export
+from as_export import *
+from secrets import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_aspace_connection(self):
+        aspace_connection = as_export.ASpace(as_un, as_pw, as_api)
+        connection_status = aspace_connection.aspace_login()
+        print(connection_status)
+        self.assertEqual(connection_status, 200)  # need to change this to receive just some status, could be incorrect
+
+    # test for grabbing archival objects get_archobjs
 
 
 if __name__ == '__main__':
